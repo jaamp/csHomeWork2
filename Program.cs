1ac8c66cc3b3cc8c147bb8d77847c989ad216a3d
jaamp <jtowery@uw.edu>
2019-05-06 11:41:05 -0700

    adding hw2 - Towery

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csHomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = new List<Models.User>();
+            users.Add(new Models.User { Name = "Dave", Password = "hello" });
+            users.Add(new Models.User { Name = "Steve", Password = "steve" });
+            users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+
+            var badPW = users.Where(p => p.Password == "hello");
+            foreach (var p in badPW)
+            {
+                Console.WriteLine($"The user {p.Name} has the bad password 'hello'");
+            }
+            var removeBadPW = badPW.FirstOrDefault();
+            users.Remove(removeBadPW);
+
+            var otrBadPW = users.Where(p => (p.Password.ToUpper() == p.Name.ToUpper()));
+            var removeOtrBadPW = otrBadPW.FirstOrDefault();
+            users.Remove(removeOtrBadPW);
+
+            foreach (var u in users)
+            {
+                Console.WriteLine($"\nThe remaining user: {u.Name} and password: {u.Password}\n");
+            }
+
+        }
+    }
+}
